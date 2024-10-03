42d6965923f2e620c2611d47ee2c1edcf5d58e1f
Lloyd Francis <lloydaf@gmail.com>
2024-10-03 20:56:43 +0200

    feat: adding strikethrough for done items

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { DeleteItem, Item, ToggleItemCheck } from "../types/types";
+import './Item.css'
 
 export const ItemComponent = ({item, toggleItemCheck, deleteItem}: {
     item: Item,
@@ -17,7 +18,7 @@ export const ItemComponent = ({item, toggleItemCheck, deleteItem}: {
         <div style={{margin: '0 1rem'}}>
             <input type="checkbox" checked={item.done}
                    onChange={e => toggleItemCheck({checked: e.target.checked, item})}/>
-            <span style={{margin: '0 0.5rem'}}>{item.name}</span>
+            <span className={item.done ? "strikethrough" : ""} style={{margin: '0 0.5rem'}}>{item.name}</span>
         </div>
         <div style={{margin: '0 1rem'}}>
             <FontAwesomeIcon icon={faTrash} onClick={() => deleteItem(item)}/>
